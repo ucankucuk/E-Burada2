24acee824bfe3c789ce8eeaf55a27d7551c98ffe
Kbhkn <hakan.kabasakal@bil.omu.edu.tr>
2016-06-17 06:06:43 +0300

    Fix 2

--- src/Util/HibernateUtil.java
+++ src/Util/HibernateUtil.java
@@ -9,7 +9,7 @@ public class HibernateUtil {
 
 	static {
 		try {
-			sessionFactory = new AnnotationConfiguration().configure("/hibernate.cfg.xml").buildSessionFactory();
+			sessionFactory = new AnnotationConfiguration().configure("/resources/hibernate.cfg.xml").buildSessionFactory();
 		} catch (Throwable ex) {
 			throw new ExceptionInInitializerError(ex);
 		}
